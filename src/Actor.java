78c303947760faecdc8ec5d0264d0e169be08a1d
sohomkroy <sohomkroy@gmail.com>
2020-04-20 21:15:32 -0700

    Finished Everything except slide 11 and 12

--- src/Actor.java
+++ src/Actor.java
@@ -1,2 +1,55 @@
-public class Actor {
+import javafx.scene.Node;
+import javafx.scene.image.ImageView;
+
+import java.util.ArrayList;
+
+public abstract class Actor extends ImageView {
+    public Actor() {}
+
+    public void move(double dx, double dy) {
+        setX(getX()+dx);
+        setY(getY()+dy);
+    }
+
+    public World getWorld() {
+        return (World) getParent();
+    }
+
+    public double getWidth() {
+        return getBoundsInParent().getWidth();
+    }
+
+    public double getHeight() {
+        return getBoundsInParent().getHeight();
+    }
+
+    public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls){
+        ArrayList<A> list = new ArrayList<A>();
+        for (Node actor : getWorld().getChildren()) {
+            if (this.intersects(actor.getBoundsInLocal())) {
+                if (cls.isInstance(actor)) {
+                    if (actor != this) {
+                        list.add(cls.cast(actor));
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
+    public <A extends Actor> A getOneIntersectingObject(java.lang.Class<A> cls) {
+        ArrayList<A> list = new ArrayList<A>();
+        for (Node actor : getWorld().getChildren()) {
+            if (this.intersects(actor.getBoundsInLocal())) {
+                if (cls.isInstance(actor)) {
+                    if (actor != this) {
+                        return cls.cast(actor);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public abstract void act(long now);
 }
--- src/World.java
+++ src/World.java
@@ -1,2 +1,45 @@
-public class World {
+import javafx.animation.AnimationTimer;
+import javafx.scene.Node;
+import javafx.scene.layout.Pane;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class World extends Pane {
+    private AnimationTimer timer;
+
+    World() {
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                act(now);
+                for (Node actor: getChildren()) {
+                    ((Actor)actor).act(now);
+                }
+            }
+        };
+    }
+    public void start() {
+        timer.start();
+    }
+    public void stop() {
+        timer.stop();
+    }
+    public void add(Actor actor) {
+        getChildren().add(actor);
+    }
+    public void remove(Actor actor){
+        getChildren().remove(actor);
+    }
+
+    public <A extends Actor> List<A> getObjects(Class<A> cls){
+        ArrayList<A> list =  new ArrayList<A>();
+        for (Node actor: getChildren()) {
+            if (cls.isInstance(actor)) {
+                list.add(cls.cast(actor));
+            }
+        }
+        return list;
+    }
+    public abstract void act(long now);
 }
